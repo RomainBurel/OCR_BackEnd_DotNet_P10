60addba3720d7b0ac0a5055d903570207304a6cb
Romain BUREL <generic3@diademe.fr>
2025-02-16 18:06:34 +0100

    Add Frontend project + first draw of frontend without styling + direct link with PatientsAPI and IdentityAPI addresses

---
+++ Frontend/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace Frontend.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int GenderId { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
